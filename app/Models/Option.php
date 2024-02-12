5f8b1c42fd9bac8f57038c2c497590b21fbca89f
Matheus Vidal Nunes <matheusvidal2012@gmail.com>
2024-02-12 14:01:28 -0300

    Listing Polls Component

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Option extends Model
 {
@@ -14,8 +15,8 @@ class Option extends Model
         return $this->belongsTo(Poll::class);
     }
 
-    public function votes(): BelongsTo{
-        return $this->belongsTo(Vote::class);
+    public function votes(): HasMany{
+        return $this->HasMany(Vote::class);
     }
 
 }
